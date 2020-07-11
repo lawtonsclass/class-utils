d708d262be95befdd35e3f0fb8bf08275d1fcd70
Lawton Nichols <lawtonnichols@gmail.com>
2020-07-10 22:15:21 -0700

    .

--- server-sbin/lawtonnichols/refresh-class-utils.sh
+++ server-sbin/lawtonnichols/refresh-class-utils.sh
@@ -16,14 +16,13 @@ chmod -R 755 server-bin
 chown -R lawtonnichols:teacher server-sbin/lawtonnichols/*
 chown -R autograder:bot server-sbin/autograder/*
 
-# put special permissions and setuid on /usr/local/bin files
+# put special permissions on /usr/local/bin files
 gcc server-bin/refresh-starter-code.c -o server-bin/refresh-starter-code
 chown root server-bin/refresh-starter-code
-chmod u+s server-bin/refresh-starter-code
 
 gcc server-bin/finish-turnin.c -o server-bin/finish-turnin
 chown root server-bin/finish-turnin
-chmod u+s server-bin/finish-turnin
+
 
 # put all the files in their proper places
 cp server-bin/* /usr/local/bin
@@ -34,6 +33,10 @@ cp server-sbin/lawtonnichols/* ~lawtonnichols/bin
 chown -R autograder:bot ~autograder/bin
 chown -R lawtonnichols:teacher ~lawtonnichols/bin
 chmod -R 700 ~autograder/bin ~lawtonnichols/bin
+
+# setuid for /usr/local/bin files because copying destroys them
+chmod u+s /usr/local/bin/refresh-starter-code
+chmod u+s /usr/local/bin/finish-turnin
 
 # put special permissions and setuid on autograder's and lawtonnichols's files
 # chmod u+s ~autograder/bin/autograde
