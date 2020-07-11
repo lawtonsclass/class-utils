11718b23ae27b36cce6bdd6ffbfb85aedadcf2f2
Lawton Nichols <lawtonnichols@gmail.com>
2020-07-10 22:08:31 -0700

    .

---
+++ server-bin/refresh-starter-code.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[])
+{
+    char *envp[] =
+    {
+        "PATH=/bin:/usr/bin",
+        0
+    };
+    setuid(0);
+    execve("/usr/local/bin/refresh-starter-code.sh", argv + 1, envp);
+    fprintf(stderr, "Couldn't run program! This shouldn't happen--please yell at Lawton.\n");
+    return 1;
+}
