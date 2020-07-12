305e241f5f78a8de86f798ae19922c4791633420
Lawton Nichols <lawtonnichols@gmail.com>
2020-07-11 17:25:25 -0700

    .

--- server-sbin/autograder/make-autograder-folder-for-user.sh
+++ server-sbin/autograder/make-autograder-folder-for-user.sh
@@ -10,4 +10,4 @@ user_home_folder=$(eval echo ~$user)
 
 mkdir -p $user_home_folder/.autograder/previous_submissions
 chown -R $user:student $user_home_folder/.autograder
-chmod -R 600 $user_home_folder/.autograder
+chmod -R 500 $user_home_folder/.autograder
